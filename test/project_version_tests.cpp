89bae10e1a52271985a2fed9a1a699f9299db044
Aymeric Pellé <aympelle@gmail.com>
2025-04-28 13:16:45 +0200

    v0.3.0: Start dev version.

--- test/project_version_tests.cpp
+++ test/project_version_tests.cpp
@@ -5,7 +5,7 @@
 TEST(project_version_tests, test_version_core)
 {
     constexpr unsigned major = 0;
-    constexpr unsigned minor = 2;
+    constexpr unsigned minor = 3;
     constexpr unsigned patch = 0;
     static_assert(arba::stdx::version.core() == arba::cppx::numver(major, minor, patch));
 }
